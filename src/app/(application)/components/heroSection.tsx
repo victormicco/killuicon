dd45379e0bb5f0f8155cf5f86d013779880ecce8
Victor Micco <victor.oficial093@gmail.com>
2024-03-13 19:10:02 -0300

    feat: new animations w/ framer motion

--- src/app/(application)/components/heroSection.tsx
+++ src/app/(application)/components/heroSection.tsx
@@ -10,6 +10,7 @@ import { apiIcon } from "../../../lib/ky";
 import { Download, Search } from "lucide-react";
 import Cards from "../../../components/card";
 import { useLocalStorage } from "@uidotdev/usehooks";
+import { motion } from "framer-motion";
 
 import Image from "next/image";
 import React from "react";
@@ -62,66 +63,84 @@ export default function HeroSection({ defaultValues }: GetIconProps) {
   return (
     <>
       <Background>
-        <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48 h-4/6">
-          <div className="container px-4 md:px-6">
-            <div className="flex flex-col items-center space-y-4 text-center">
-              <div className="space-y-2">
-                <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none mb-10">
-                  Ache qualquer{" "}
-                  <span className="underline decoration-violet-500 decoration-wavy decoration-from-font underline-offset-4">
-                    ícone
-                  </span>{" "}
-                  com o{" "}
-                  <span className="bg-gradient-to-r from-cyan-400 via-blue-900 to-purple-600 bg-clip-text  font-extrabold tracking-tighter text-transparent">
-                    Killuicon
-                  </span>
-                  ⚡
-                </h1>
-                <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
-                  Apenas coloque o domínio do site que deseja obter o ícone e
-                  veja a mágica!
-                </p>
-              </div>
-              <div className="w-full max-w-sm space-y-2 pt-4">
-                <Form {...form}>
-                  <form className="flex space-x-2">
-                    <Form.Field
-                      control={form.control}
-                      name="domain"
-                      render={({ field }) => (
-                        <TextFieldInput
-                          className="max-w-lg flex-1"
-                          placeholder="Ex: youtube.com.br"
-                          type="text"
-                          {...field}
-                        />
-                      )}
+        <motion.div
+          initial={{ y: 10, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          exit={{ y: -10, opacity: 0 }}
+          transition={{ duration: 0.2 }}
+        >
+          <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48 h-4/6">
+            <div className="container px-4 md:px-6">
+              <div className="flex flex-col items-center space-y-4 text-center">
+                <div className="space-y-2">
+                  <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none mb-10">
+                    Ache qualquer{" "}
+                    <span className="underline decoration-violet-500 decoration-wavy decoration-from-font underline-offset-4">
+                      ícone
+                    </span>{" "}
+                    com o{" "}
+                    <span className="bg-gradient-to-r from-cyan-400 via-blue-900 to-purple-600 bg-clip-text  font-extrabold tracking-tighter text-transparent">
+                      Killuicon
+                    </span>
+                    ⚡
+                  </h1>
+                  <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
+                    Apenas coloque o domínio do site que deseja obter o ícone e
+                    veja a mágica!
+                  </p>
+                </div>
+                <div className="w-full max-w-sm space-y-2 pt-4">
+                  <Form {...form}>
+                    <form className="flex space-x-2">
+                      <Form.Field
+                        control={form.control}
+                        name="domain"
+                        render={({ field }) => (
+                          <TextFieldInput
+                            className="max-w-lg flex-1"
+                            placeholder="Ex: youtube.com.br"
+                            type="text"
+                            {...field}
+                          />
+                        )}
+                      />
+                      <Button
+                        type="button"
+                        variant="outline"
+                        onClick={handleSearchIcon}
+                      >
+                        Buscar Ícone
+                        <Search className="h-4 w-4 ml-3" />
+                      </Button>
+                    </form>
+                  </Form>
+                </div>
+                {formData ? (
+                  <Cards iconUrl={formData} />
+                ) : (
+                  <motion.div
+                    className="box"
+                    initial={{ opacity: 0, scale: 0.5 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{
+                      duration: 0.8,
+                      delay: 0.5,
+                      ease: [0, 0.71, 0.2, 1.01],
+                    }}
+                  >
+                    <Image
+                      alt="Killuicon"
+                      src="https://i.pinimg.com/originals/9f/99/59/9f9959c3e16d740c62fb1fe250ed67c3.gif"
+                      width={500}
+                      height={500}
+                      className="rounded-xl pt-7 w-auto h-auto max-w-[500px] max-h-[500px]"
                     />
-                    <Button
-                      type="button"
-                      variant="outline"
-                      onClick={handleSearchIcon}
-                    >
-                      Buscar Ícone
-                      <Search className="h-4 w-4 ml-3" />
-                    </Button>
-                  </form>
-                </Form>
+                  </motion.div>
+                )}
               </div>
-              {formData ? (
-                <Cards iconUrl={formData} />
-              ) : (
-                <Image
-                  alt="Killuicon"
-                  src="https://i.pinimg.com/originals/9f/99/59/9f9959c3e16d740c62fb1fe250ed67c3.gif"
-                  width={500}
-                  height={500}
-                  className="rounded-xl pt-7 w-auto h-auto max-w-[500px] max-h-[500px]"
-                />
-              )}
             </div>
-          </div>
-        </section>
+          </section>
+        </motion.div>
       </Background>
     </>
   );
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -4,6 +4,7 @@ import { Card } from "@fellipeutaka/ui/card";
 import { Download } from "lucide-react";
 import { useEffect, useState } from "react";
 import { getIconService } from "../services";
+import { motion } from "framer-motion";
 
 export default function Cards({ iconUrl }: { iconUrl: string }) {
   const [title, setTitle] = useState(iconUrl);
@@ -38,30 +39,52 @@ export default function Cards({ iconUrl }: { iconUrl: string }) {
   }
 
   return (
-    <Card className="w-64 max-w-sm rounded-xl border-t dark:border-gray-800 backdrop-blur-sm">
-      <div className="relative aspect-square">
-        <img
-          alt="Image"
-          className="object-cover rounded-3xl p-5"
-          src={iconUrl}
-          style={{
-            aspectRatio: "400/400",
-            objectFit: "cover",
+    <motion.div
+      initial={{ y: 10, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      exit={{ y: -10, opacity: 0 }}
+      transition={{ duration: 0.2 }}
+    >
+      <Card className="w-64 max-w-sm rounded-xl border-t dark:border-gray-800 backdrop-blur-sm">
+        <motion.div
+          className="box"
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 0.8,
+            delay: 0.5,
+            ease: [0, 0.71, 0.2, 1.01],
           }}
-        />
-      </div>
-      <Card.Content className="p-6">
-        <div className="grid gap-2">
-          <Card.Title className="text-base w-20">{lastWord}</Card.Title>
-          <Card.Description className="text-sm"></Card.Description>
-        </div>
-      </Card.Content>
-      <Card.Footer className="p-6 flex justify-center">
-        <Button onClick={handleDownloadIcon}>
-          Baixar ícone
-          <Download className="ml-2 h-4 w-4" />
-        </Button>
-      </Card.Footer>
-    </Card>
+        >
+          <div className="relative aspect-square">
+            <img
+              alt="Image"
+              className="object-cover rounded-3xl p-5"
+              src={iconUrl}
+              style={{
+                aspectRatio: "400/400",
+                objectFit: "cover",
+              }}
+            />
+          </div>
+        </motion.div>
+        <Card.Content className="p-6">
+          <div className="grid gap-2">
+            <Card.Title className=" w-20 text-center text-lg ml-10">
+              {lastWord}
+            </Card.Title>
+            <Card.Description className="text-sm"></Card.Description>
+          </div>
+        </Card.Content>
+        <Card.Footer className="p-6 flex justify-center">
+          <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+            <Button onClick={handleDownloadIcon}>
+              Baixar ícone
+              <Download className="ml-2 h-4 w-4" />
+            </Button>
+          </motion.div>
+        </Card.Footer>
+      </Card>
+    </motion.div>
   );
 }
