ce59e2c075dd44d054542d371340800c5c419228
Victor Micco <victor.oficial093@gmail.com>
2024-03-27 19:53:53 -0300

    fix:Theme issue

--- src/app/(application)/layout.tsx
+++ src/app/(application)/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="black"
           enableSystem
           disableTransitionOnChange
         >
