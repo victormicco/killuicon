1e06f6b78e5985470633ee1207b59c38b1ee1718
henrique-anjun <henrique.moreira@anjunexpress.com.br>
2024-04-04 08:22:36 -0300

    fix(layout): theme conflict

--- src/app/(application)/layout.tsx
+++ src/app/(application)/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../../styles/globals.css";
 import { ThemeProvider } from "../../components/theme-provider";
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
+import { Background } from "@/components/background";
+import { Toaster } from "@fellipeutaka/ui/toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,19 +17,22 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode; 
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="pt-BR">
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="black"
+          defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <Navbar />
+          <Background>{children}</Background>
+          <Footer />
         </ThemeProvider>
+        <Toaster />
       </body>
     </html>
   );
--- src/app/(application)/page.tsx
+++ src/app/(application)/page.tsx
@@ -1,15 +1,9 @@
-import { Button } from "@fellipeutaka/ui/button";
 import HeroSection from "./components/heroSection";
-import { ModeToggle } from "../../components/setThemeToggle";
-import Navbar from "../../components/navbar";
-import Footer from "../../components/footer";
 
 export default function Page() {
   return (
     <>
-      <Navbar />
       <HeroSection />
-      <Footer />
     </>
   );
 }
